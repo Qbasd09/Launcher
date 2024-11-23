59f92955b1f2783e60679fb16d1cdff0c4d51dbc
Qbasd09 <oleksandras69420@gmail.com>
2024-11-23 06:42:22 +0100

    Drag and Lift have been bound to eachother

--- launch_with_drag.py
+++ launch_with_drag.py
@@ -1,11 +1,15 @@
 import math
 import csv
+import numpy as np
 
+v = 9.67
 ro = 1.204
 A = 0.03
 g = -9.81
 m = 0.035
 F_g = m * g
+coefficient = ro * A /(2 * m)
+
 
 with open('alpha_to_cl.csv', newline='') as f:
     reader = csv.reader(f)
@@ -16,35 +20,19 @@ for i in data:
     Alpha = float(i[0])
     Cl = float(i[1])
     Cd = float(i[2])
+    RadAlpha = math.radians(Alpha)
+    vHor = v * math.cos(RadAlpha)
     
-    F_lift = 1000 
-    v = 8
+    t = np.linspace(0, 50, 500)
 
-    while abs(F_g) < F_lift:
-        RadAlpha = math.radians(Alpha)
-        vHor = v * math.cos(RadAlpha)
-
-        F_lift = Cl * ro * vHor**2 * A * 0.5
-
-        v -= 0.01
-
-    a = F_lift/m
-    a_v = abs(g+a)
+    def vHor_t(Cd, t):
+        speed = 1 / (coefficient * t * Cd + 1/vHor)
     
-    if Alpha == 0:
-        F_lift0 = F_lift
-        a_0 = a
-        a_v0 = a_v
+        return speed    
 
-    h0 = 1.5
-    h = 0
-    vVer = v * math.sin(RadAlpha)
-    tLaunchAngle = 2
+    def F_lift(Cl, t):
+        lift = Cl * ro * vHor_t(Cd, t)**2 * A * 0.5
 
-    h1 = h0 + vVer * tLaunchAngle - 0.5 * a_v * tLaunchAngle**2
-    vVer1 = 0
-    d1 = vHor * tLaunchAngle
-
-    d = vHor*((vVer1 + (vVer1**2 + 2 * h1 * a_v0)**0.5)/a_v0) + d1
-
-    print(f"{Alpha:.2f}, {h1:.2f}, {a_v:.2f}, {h1:.2f}, {d1:.2f}, {d:.2f}, {vHor:.2f}, {vVer:.2f}")
+        return lift
+    
+    print(F_lift(Cl, t))
