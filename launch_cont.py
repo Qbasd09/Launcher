738d5a6bfcf8fcc2384bffe0c7408efe20563177
Qbasd09 <oleksandras69420@gmail.com>
2024-11-28 03:24:28 +0100

    Did a big fucky wucky in the velocty calculations, I think I have fixed it now

--- launch_cont.py
+++ launch_cont.py
@@ -33,31 +33,32 @@ for i in data:
     h = 0
     tLaunchAngle = 1
 
-    tl0 = 0.01 * tLaunchAngle
+    tl0 = 0.001 * tLaunchAngle
     tl1 = tl0
     d1 = 0
 
     while tl0 <= tLaunchAngle:
-        tl0 += 0.01 * tLaunchAngle
-        vHor1 = 1 / (coef * tl0 * Cd + 1/vHor1)
+        tl0 += 0.001 * tLaunchAngle
+        vHor1 = 1 / (coef * tl0 * Cd + 1/vHor)
         d1 = vHor1 * tl1 + d1
 
     # d1 = vHor * tLaunchAngle
     h1 = h0 + vVer * tLaunchAngle
 
     vHor2 = 1 / (coef * tLaunchAngle * Cd + 1/vHor)
+    vHor3 = vHor2
     vVer1 = 0
     a = 1
 
     t = (2 * h1/a)**0.5
 
-    t0 = 0.01 * t
+    t0 = 0.001 * t
     t1 = t0
     d2 = 0
 
     while t0 <= t:
-        t0 += 0.01 * t
-        vHor2 = 1 / (coef * t0 * Cd0 + 1/vHor2)
+        t0 += 0.001 * t
+        vHor2 = 1 / (coef * t0 * Cd0 + 1/vHor3)
         d2 = vHor2 * t1 + d2
 
     d = d1 + d2
